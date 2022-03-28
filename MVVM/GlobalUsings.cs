9b0175f650f61c4879f740c4a525aab932c6cbe9
dimainnature <dimainnature@yandex.ru>
2022-03-29 00:13:44 +0400

    Add MVVM infrastructure files.

--- MVVM/GlobalUsings.cs
+++ MVVM/GlobalUsings.cs
@@ -1,1 +1,3 @@
-﻿
+﻿global using System;
+global using System.ComponentModel;
+global using System.Windows.Input;
---
+++ MVVM/ViewModel/BaseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MVVM.ViewModel;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void OnPropertyChanged(string propertyName) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+}
